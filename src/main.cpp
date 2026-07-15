27706d6aae179eb7e01a47b94edf50f4b0f186d8
Axxodes <seppeaxolotlking@gmail.com>
2026-07-15 22:33:03 +0200

    learning cpp functions twin

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+int refDoSomething() 
+{
+	int x {};
+
+	std::cin >> x;
+
+	return x;
+}
+
 int main()
 {
-	int x{};
-	int y{};
-
-	std::cout << "Enter two numbers seperated by a space:" << "\n";
-
-	std::cin >> x >> y;
-
-	std::cout << "You chose: " << x << " and " << y << "\n";
-
-	std::cout << sizeof(int);
-
-    return 0;
+	int num{ refDoSomething() };
+	
+	std::cout << num*2 << "\n";
 }
