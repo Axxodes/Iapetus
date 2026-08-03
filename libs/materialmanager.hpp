b9d5e22baef8af4103b2fe44ef268b172f44a044
Axxodes <300546800+Axxodes@users.noreply.github.com>
2026-08-03 14:40:21 +0200

    fileservice
    thanks to cakez on youtube for this awesome tutorial, https://www.youtube.com/watch?v=9R2rRLbBkHU

--- libs/materialmanager.hpp
+++ libs/materialmanager.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <array>
+#include <fstream>
+#include <iostream>
 
 class material 
 {
@@ -24,3 +26,16 @@ class material
 		}
 	}
 };
+
+void generateBlankAsset()
+{
+    // make it a .png or wtv we use later lol 
+
+    std::ofstream file {};
+    file.open("Asset.txt");
+    if (file.is_open())
+    {
+        file << "NO SHOUTOUT TO SPIDER";
+        file.close();
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,13 @@
 #include "../libs/propertiesclasses.hpp"
 #include "../libs/objects.hpp"
 #include "../libs/window.hpp"
+#include "../libs/services.hpp"
 
 int main()
 {
 	platform_create_window(1200,720,"Iapetus");
+
+	generateBlankAsset();
 
 	while(running)
 	{
